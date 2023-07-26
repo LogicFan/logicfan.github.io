027160140c2c927e09a53e06c635f1b3e35acaed
Logic <fanyongda2012@gmail.com>
2023-07-25 23:23:12 -0400

    update data

---
+++ etl/infection.py
@@ -0,0 +1,30 @@
+# %% setup
+import pandas
+
+# %% import infection data
+infection_2020 = pandas.read_csv("./infection/us-counties-2020.csv")
+infection_2021 = pandas.read_csv("./infection/us-counties-2021.csv")
+infection_2022 = pandas.read_csv("./infection/us-counties-2022.csv")
+infection_2023 = pandas.read_csv("./infection/us-counties-2023.csv")
+
+infection = pandas.concat(
+    [
+        infection_2020,
+        infection_2021,
+        infection_2022,
+        infection_2023,
+    ]
+)
+
+infection
+
+# %% process
+df = infection[["date", "state", "cases", "deaths"]]
+df = df.groupby(["date", "state"]).sum()
+df = df.reset_index()
+
+df
+
+# %%
+df.to_csv("infection.csv")
+# %%
